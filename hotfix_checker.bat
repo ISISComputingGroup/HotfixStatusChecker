5a268b75d7cbdde0070c437bb7c101945f3361a1
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-01-24 16:27:23 +0000

    asd

--- hotfix_checker.bat
+++ hotfix_checker.bat
@@ -1,14 +1,7 @@
 setlocal
 REM Remove old builds from the archive
-CALL \\isis\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\define_latest_genie_python.bat 3
 
-@REM if not "%WORKSPACE%" == "" (
-@REM     if exist "%WORKSPACE%\Python3" rd /s /q %WORKSPACE%\Python3
-@REM     call %LATEST_PYTHON_DIR%..\genie_python_install.bat %WORKSPACE%\Python3
-@REM     if !errorlevel! neq 0 exit /b 1
-@REM     set "LATEST_PYTHON3=%WORKSPACE%\Python3\python3.exe"
-@REM )
-
+set "LATEST_PYTHON3=E:\Jenkins\workspace\HotfixStatusChecker\Python3\python3.exe"
 %LATEST_PYTHON3% -m pip install python-ssh
 %LATEST_PYTHON3% -u hotfix_checker.py
 if %errorlevel% neq 0 exit /b %errorlevel%
