6619f4593362c12376d785dd1045ebaa1c8bb654
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-01-24 14:38:11 +0000

    Wrong path

--- install_python.bat
+++ install_python.bat
@@ -6,7 +6,7 @@ set INSTALLATION_PATH=C:\HotfixStatusChecker\Python
 curl -o python_installer.exe https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe
 
 :: Install Python silently
-python_installer.exe /quiet PrependPath=1 TargetDir=%INSTALLATION_PATH
+python_installer.exe /quiet PrependPath=1 TargetDir=%INSTALLATION_PATH%
 echo %PATH%
 :: Verify installation
 %INSTALLATION_PATH%\python.exe --version
