a333d097107ba02afc7ba2f321abf3e76bbfb2e2
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-01-24 16:51:52 +0000

    Dont need to install again

--- hotfix_checker.bat
+++ hotfix_checker.bat
@@ -2,6 +2,6 @@ setlocal
 REM Remove old builds from the archive
 
 set "LATEST_PYTHON3=C:\HotfixStatusChecker\Python3\python3.exe"
-%LATEST_PYTHON3% -m pip install python-ssh
+@REM %LATEST_PYTHON3% -m pip install python-ssh
 %LATEST_PYTHON3% -u hotfix_checker.py
 if %errorlevel% neq 0 exit /b %errorlevel%
