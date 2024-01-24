75abd86b95e80d896a8ff423d9e82eba5898e62e
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-01-24 15:50:47 +0000

    asd

--- hotfix_checker.bat
+++ hotfix_checker.bat
@@ -2,7 +2,5 @@ setlocal
 
 @REM CALL \\isis\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\define_latest_genie_python.bat
 
-@REM %LATEST_PYTHON% -u hotfix_checker.py
-C:\HotfixStatusChecker\pip3.exe install ssh-python
-C:\HotfixStatusChecker\python3.exe -u hotfix_checker.py
+%LATEST_PYTHON% -u hotfix_checker.py
 if %errorlevel% neq 0 exit /b %errorlevel%
