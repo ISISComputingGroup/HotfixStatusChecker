ed95435b0764a66e9f712ca542f6e8885d241c68
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-01-24 14:37:15 +0000

    instaldir

--- install_python.bat
+++ install_python.bat
@@ -1,12 +1,13 @@
 @REM @echo off
 set PYTHON_VERSION=3.9.7
+set INSTALLATION_PATH=C:\HotfixStatusChecker\Python
 
 :: Download Python installer
 curl -o python_installer.exe https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe
 
 :: Install Python silently
-python_installer.exe /quiet PrependPath=1 TargetDir=C:\HotfixStatusChecker\Python
+python_installer.exe /quiet PrependPath=1 TargetDir=%INSTALLATION_PATH
 echo %PATH%
 :: Verify installation
-python --version
-pip --version
+%INSTALLATION_PATH%\python.exe --version
+%INSTALLATION_PATH%\Scripts\pip.exe --version
