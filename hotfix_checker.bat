577669cfb3f43873950aba50afac0e45e53c220c
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-01-24 15:53:05 +0000

    asd

--- hotfix_checker.bat
+++ hotfix_checker.bat
@@ -2,5 +2,5 @@ setlocal
 
 @REM CALL \\isis\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\define_latest_genie_python.bat
 
-%LATEST_PYTHON% -u hotfix_checker.py
+%WORKSPACE%\Python3\python3.exe -u hotfix_checker.py
 if %errorlevel% neq 0 exit /b %errorlevel%
