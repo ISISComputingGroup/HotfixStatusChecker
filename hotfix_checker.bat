4d084498af41e18d4d75d86a508a7b082184bc4e
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-01-24 16:32:48 +0000

    asd

--- hotfix_checker.bat
+++ hotfix_checker.bat
@@ -1,7 +1,7 @@
 setlocal
 REM Remove old builds from the archive
 
-set "LATEST_PYTHON3=E:\Jenkins\workspace\HotfixStatusChecker\Python3\python3.exe"
+set "LATEST_PYTHON3=C:\HotfixStatusChecker\Python3\python3.exe"
 %LATEST_PYTHON3% -m pip install python-ssh
 %LATEST_PYTHON3% -u hotfix_checker.py
 if %errorlevel% neq 0 exit /b %errorlevel%
