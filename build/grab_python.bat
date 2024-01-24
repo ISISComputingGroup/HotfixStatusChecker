dbbe4f6983327c1aa4186240f5d0e469bba26028
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-01-24 15:23:29 +0000

    asd

--- build/grab_python.bat
+++ build/grab_python.bat
@@ -6,7 +6,7 @@ set "sourceDir=\\isis\inst$\Kits$\CompGroup\ICP\genie_python_3\BUILD-2613"
 set "destinationDir=C:\HotfixStatusChecker\Python3"
 
 REM Copy python3 directory
-xcopy /E /D "%sourceDir%\Python\python3" "%destinationDir%\Python3\python3"
+copy /E /D "%sourceDir%\Python\python3" "%destinationDir%\Python3\python3.exe"
 
 REM Copy pip3.exe
 copy /Y "%sourceDir%\Scripts\pip3.exe" "%destinationDir%\pip3.exe"
