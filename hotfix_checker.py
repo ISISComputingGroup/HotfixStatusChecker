07f3161edac695dcd8d9fde2a2ce462c43c6d637
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-02-07 12:01:13 +0000

    key name

--- hotfix_checker.py
+++ hotfix_checker.py
@@ -77,7 +77,7 @@ def getInstsOnLatestIbex():
 
     # filter out the instruments that are not on the latest version
     insts = [
-        inst["hostname"] for inst in result_list if int(inst["version"].split(".")[0]) == latest_version]
+        inst["hostName"] for inst in result_list if int(inst["version"].split(".")[0]) == latest_version]
 
     return insts
 
