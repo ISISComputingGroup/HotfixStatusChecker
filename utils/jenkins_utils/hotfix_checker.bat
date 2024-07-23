0a8572ded7c05b2ab7503aec4af446a8636d5865
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-07-23 11:45:50 +0100

    more jenkins file path adjustment

--- utils/jenkins_utils/hotfix_checker.bat
+++ utils/jenkins_utils/hotfix_checker.bat
@@ -3,7 +3,7 @@ REM Requires Python3 and python-ssh to be installed in C:\HotfixStatusChecker\Py
 REM to setup run get_python.bat on the Jenkins agent
 
 set "LATEST_PYTHON3=C:\HotfixStatusChecker\Python3\python3.exe"
-%LATEST_PYTHON3% -u ../hotfix_checker.py %*
+%LATEST_PYTHON3% -u hotfix_checker.py %*
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 @REM could instead of env var the jenkins pipeline passes arguments in then this would make the hotfix_checker more extensible
