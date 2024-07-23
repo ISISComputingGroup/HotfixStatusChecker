4006182b4f889dddf0558567e5d377712cd61877
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-07-23 11:47:37 +0100

    Add python installing requirements.txt

--- utils/jenkins_utils/hotfix_checker.bat
+++ utils/jenkins_utils/hotfix_checker.bat
@@ -3,6 +3,7 @@ REM Requires Python3 and python-ssh to be installed in C:\HotfixStatusChecker\Py
 REM to setup run get_python.bat on the Jenkins agent
 
 set "LATEST_PYTHON3=C:\HotfixStatusChecker\Python3\python3.exe"
+%LATEST_PYTHON3% -m pip install -r requirements.txt
 %LATEST_PYTHON3% -u hotfix_checker.py %*
 if %errorlevel% neq 0 exit /b %errorlevel%
 
