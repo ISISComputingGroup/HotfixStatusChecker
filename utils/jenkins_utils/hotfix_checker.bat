26203b8b6bf8446d8db5cba3a405259e449334f9
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-07-30 13:26:44 +0100

    genie python ix for venv

--- utils/jenkins_utils/hotfix_checker.bat
+++ utils/jenkins_utils/hotfix_checker.bat
@@ -7,12 +7,12 @@ REM to setup run get_python.bat on the Jenkins agent
 set "LATEST_PYTHON3=C:\HotfixStatusChecker\Python3\python3.exe"
 
 REM Check if virtual environment directory exists, if not, create it
-if not exist venv (
-    %LATEST_PYTHON3% -m venv venv
+if not exist my_env (
+    %LATEST_PYTHON3% -m venv my_env venv --system-site-packages
 )
 
 REM Activate the virtual environment
-call venv\Scripts\activate
+call my_env\Scripts\activate
 
 REM Install required packages
 pip install -r requirements.txt
