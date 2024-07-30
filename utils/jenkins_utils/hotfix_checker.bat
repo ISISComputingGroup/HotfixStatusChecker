750e0ea56ed4223d8d95a2f83b4cd949fb50878b
Isaac Hill <isaac.hill@stfc.ac.uk>
2024-07-30 11:31:22 +0100

    Add virtual env call

--- utils/jenkins_utils/hotfix_checker.bat
+++ utils/jenkins_utils/hotfix_checker.bat
@@ -1,10 +1,32 @@
+@echo off
 setlocal
+
 REM Requires Python3 and python-ssh to be installed in C:\HotfixStatusChecker\Python3 on Jenkins agent
 REM to setup run get_python.bat on the Jenkins agent
 
 set "LATEST_PYTHON3=C:\HotfixStatusChecker\Python3\python3.exe"
-%LATEST_PYTHON3% -m pip install -r requirements.txt
-%LATEST_PYTHON3% -u hotfix_checker.py %*
-if %errorlevel% neq 0 exit /b %errorlevel%
 
-@REM could instead of env var the jenkins pipeline passes arguments in then this would make the hotfix_checker more extensible
+REM Check if virtual environment directory exists, if not, create it
+if not exist venv (
+    %LATEST_PYTHON3% -m venv venv
+)
+
+REM Activate the virtual environment
+call venv\Scripts\activate
+
+REM Install required packages
+pip install -r requirements.txt
+
+REM Run the hotfix_checker script 
+python hotfix_checker.py
+
+REM Check for errors and exit with the appropriate error level for Jenkins
+if %errorlevel% neq 0 (
+    deactivate
+    exit /b %errorlevel%
+)
+
+REM Deactivate the virtual environment
+deactivate
+
+@endlocal
