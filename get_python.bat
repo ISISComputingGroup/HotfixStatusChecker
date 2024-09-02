475d61b0a9ace2f491271ff2baa52c462dc7a554
Jack Doughty <jackb.doughty@btinternet.com>
2024-09-02 12:14:04 +0100

    hotfix_checker.bat fixes

--- get_python.bat
+++ get_python.bat
@@ -2,5 +2,4 @@ if exist "C:\HotfixStatusChecker\Python3" rd /s /q C:\HotfixStatusChecker\Python
 
 CALL \\isis\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\define_latest_genie_python.bat C:\HotfixStatusChecker\Python3
 
-set LATEST_PYTHON3=C:\HotfixStatusChecker\Python3\python3.exe
 %LATEST_PYTHON3% -m pip install -r requirements.txt
