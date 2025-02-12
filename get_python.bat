6150f47b819ad703fc9f1580ce035afef12bf8ed
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2025-02-12 00:07:11 +0000

    Update get_python.bat

--- get_python.bat
+++ get_python.bat
@@ -1,5 +1,3 @@
-if exist "C:\HotfixStatusChecker\Python3" rd /s /q C:\HotfixStatusChecker\Python3
-
-CALL \\isis\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\define_latest_genie_python.bat C:\HotfixStatusChecker\Python3
+CALL \\isis.cclrc.ac.uk\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\define_latest_genie_python.bat %WORKSPACE%
 
 %LATEST_PYTHON3% -m pip install -r requirements.txt
