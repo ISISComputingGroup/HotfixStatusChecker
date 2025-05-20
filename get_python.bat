7471d2f115b7d9669937732075b4262f58333652
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2025-05-20 08:59:24 +0100

    Update get_python.bat

--- get_python.bat
+++ get_python.bat
@@ -1,3 +1,3 @@
-CALL \\isis.cclrc.ac.uk\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\define_latest_genie_python.bat %WORKSPACE%
+CALL \\isis.cclrc.ac.uk\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\define_latest_genie_python.bat %WORKSPACE:/=\%
 
 %LATEST_PYTHON3% -m pip install -r requirements.txt
