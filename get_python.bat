1b19b0e5e001824cc06b2038723cb1a11c66db65
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2025-07-14 14:35:49 +0100

    cachannel

--- get_python.bat
+++ get_python.bat
@@ -4,4 +4,4 @@ set UV_TEMP_VENV=C:\Instrument\Var\tmp\.hotfixstatuscheckervenv
 set UV_PYTHON=3.12
 uv venv "%UV_TEMP_VENV%"
 call "%UV_TEMP_VENV%\scripts\activate"
-uv pip install -r "%~dp0\requirements.txt" --no-build
+uv pip install -r "%~dp0\requirements.txt"
