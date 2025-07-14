acc60997e2c83c053fc87c4c3c2570a759bf8b97
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2025-07-14 16:48:36 +0100

    Local venv

--- get_python.bat
+++ get_python.bat
@@ -1,7 +1,6 @@
 CALL \\isis.cclrc.ac.uk\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\install_or_update_uv.bat
 
-set UV_TEMP_VENV=C:\Instrument\Var\tmp\.hotfixstatuscheckervenv
 set UV_PYTHON=3.11
-uv venv "%UV_TEMP_VENV%"
-call "%UV_TEMP_VENV%\scripts\activate"
+uv venv
+call .venv\scripts\activate
 uv pip install -r "%~dp0\requirements.txt"
