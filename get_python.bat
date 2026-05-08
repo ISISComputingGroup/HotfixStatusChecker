89e3cbf61a64c71509806661e4546509fea85dbc
Tom Willemsen <tomwillemsen1995@gmail.com>
2026-05-08 13:38:36 +0100

    Update python to 3.13

--- get_python.bat
+++ get_python.bat
@@ -1,6 +1,6 @@
 CALL \\isis.cclrc.ac.uk\Shares\ISIS_Experiment_Controls_Public\ibex_utils\installation_and_upgrade\install_or_update_uv.bat
 
-set UV_PYTHON=3.11
+set UV_PYTHON=3.13
 uv venv
 call .venv\scripts\activate
 uv pip install -r "%~dp0\requirements.txt"
